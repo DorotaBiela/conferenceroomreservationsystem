1937bc782a908d101a2566c8d618e2bfb5929d33
DorotaBiela <bilkapl@gmail.com>
2022-08-19 19:51:56 +0200

    fix(crrs): broken unit test

--- src/main/java/pl/sdacademy/ConferenceRoomReservationSystem/organization/Organization.java
+++ src/main/java/pl/sdacademy/ConferenceRoomReservationSystem/organization/Organization.java
@@ -6,8 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
